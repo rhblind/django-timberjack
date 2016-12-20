08501d8da930ca1d998b66194e8eeb91b16ce427
Rolf Håvard Blindheim <rhblind@gmail.com>
2016-12-21 00:46:12 +0100

    Updated ObjectAccessLog document

--- timberjack/models.py
+++ timberjack/models.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.admin.models import LogEntry
+
 from django.utils import timezone
-from django.utils.encoding import force_text
-from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import smart_text
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from mongoengine import *
 from mongoengine.queryset import QuerySet
@@ -29,29 +31,44 @@ LOG_LEVEL = (
 
 class ObjectAccessLogQuerySet(QuerySet):
 
-    def log_action(self, user_id, content_type_id, object_id,
-                   object_repr, action_flag, message='', write_admin_log=False):
-        raise NotImplementedError
+    def log_action(self, user_pk, content_type, object_pk, object_repr,
+                   action_flag, message, level, ip_address, write_admin_log=False):
+        raise NotImplemented
 
 
 class ObjectAccessLog(Document):
 
     meta = {
-        'db_alias': 'timberjack',
         'queryset_class': ObjectAccessLogQuerySet
     }
 
     message = StringField(default='')
     action_flag = IntField(min_value=1, max_value=4, required=True)
     level = IntField(choices=LOG_LEVEL, default=0)
-    object_pk = StringField(required=True)
+    object_pk = DynamicField(required=True)
     content_type = ContentTypeField(required=True)
+    object_repr = StringField(max_length=200, required=True)
     user_pk = UserPKField(required=False)
     ip_address = StringField(validation=validate_ip_address, required=False)
     timestamp = DateTimeField(required=True, default=timezone.now)
 
     def __repr__(self):
-        return force_text(self.timestamp)
+        return smart_text(self.timestamp)
+
+    def __str__(self):
+        if self.is_create_action:
+            return ugettext('Created "%(object)s.') % {'object': self.object_repr}
+        elif self.is_update_action:
+            return ugettext('Updated "%(object)s - %(changes)s"') % {
+                'object': self.object_repr,
+                'changes': self.get_change_message()
+            }
+        elif self.is_delete_action:
+            return ugettext('Deleted "%(object)s."') % {'object': self.object_repr}
+        elif self.is_read_action:
+            return ugettext('Read "%(object)s."') % {'object': self.object_repr}
+
+        return ugettext('ObjectAccessLog Object')
 
     @property
     def is_create_action(self):
@@ -70,10 +87,14 @@ class ObjectAccessLog(Document):
         return self.action_flag == DELETE
 
     def get_change_message(self):
-        raise NotImplementedError
+        """
+        See admin.LogEntry.
+        """
+        # TODO: Do a proper implementation
+        return self.message or 'No record!'
 
     def write_admin_log(self):
         """
         Write a copy to the django admin log.
         """
-        raise NotImplementedError
+        # LogEntry.objects.log_action(self.user_pk, self.content_type.pk, )
